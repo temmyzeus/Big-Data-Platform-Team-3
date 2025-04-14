89c663747d0a09124f9ad40064ad2e5bb5ad2378
AyoDennis <ayodejidennis33@gmail.com>
2025-04-14 16:51:40 +0100

    igw  provisioning

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -15,14 +15,52 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "big-data-platform-team-3" {
   cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
 }
 
 #Public subnet
 
-resource "aws_default_subnet" "public_subnet" {
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.big-data-platform-team-3.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "Default subnet for us-east-1a"
+    Name = "Main"
+  }
+}
+
+# Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.big-data-platform-team-3.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+# Route Table
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.big-data-platform-team-3.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "example"
   }
 }
