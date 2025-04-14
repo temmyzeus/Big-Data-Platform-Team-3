783dab1edeaf4b3c5a4271785631acbbf63d033c
AyoDennis <ayodejidennis33@gmail.com>
2025-04-14 16:49:51 +0100

    subnet provisioning

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -17,4 +17,12 @@ resource "aws_vpc" "big-data-platform-team-3" {
   cidr_block = "10.0.0.0/16"
 }
 
+#Public subnet
 
+resource "aws_default_subnet" "public_subnet" {
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Default subnet for us-east-1a"
+  }
+}
