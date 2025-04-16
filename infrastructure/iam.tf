b02ec506d0b8699ff85b62cbe81b7129458a8821
AyoDennis <ayodejidennis33@gmail.com>
2025-04-16 16:25:11 +0100

    create s3 user policy doc

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "s3_user_policy_doc" {
   }
 }
 
-resource "aws_iam_user_policy" "lb_ro" {
+resource "aws_iam_user_policy" "s3_user_policy" {
   name   = "s3_user_policy"
   user   = aws_iam_user.s3_user.name
   policy = data.aws_iam_policy_document.lb_ro.json
