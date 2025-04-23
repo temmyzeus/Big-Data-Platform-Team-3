8c954e9b904c0ecc1c34a0831bbe2a8f069df908
AyoDennis <ayodejidennis33@gmail.com>
2025-04-23 20:44:31 +0100

    terminate cluster

--- airflow/dags/emr-orchestration.py
+++ airflow/dags/emr-orchestration.py
@@ -108,3 +108,10 @@ step_checker = EmrStepSensor(
     aws_conn_id='aws_default',
     dag=dag
 )
+
+terminate_cluster = EmrTerminateJobFlowOperator(
+    task_id='terminate_cluster',
+    job_flow_id="{{ task_instance.xcom_pull(task_ids='create_emr_cluster', key='return_value') }}",
+    aws_conn_id='aws_default',
+    dag=dag
+)
