760f97c56eeaf05b2dbf76053d4e94e1ff1ce35a
AyoDennis <ayodejidennis33@gmail.com>
2025-04-23 15:59:16 +0100

    emr dag definition

--- airflow/dags/emr-orchestration.py
+++ airflow/dags/emr-orchestration.py
@@ -1,4 +1,14 @@
 from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.providers.amazon.aws.operators.emr import EmrCreateJobFlowOperator, EmrAddStepsOperator, EmrTerminateJobFlowOperator
+
+from airflow.providers.amazon.aws.operators.emr import (
+    EmrAddStepsOperator, EmrCreateJobFlowOperator, EmrTerminateJobFlowOperator)
 from airflow.providers.amazon.aws.sensors.emr import EmrStepSensor
+
+from airflow import DAG
+
+with DAG(
+    dag_id="emr_orchestration",
+    start_date=datetime(2024, 11, 22),
+    schedule_interval='0 0 * * *',
+    catchup=False
+) as dag:
