2a0b31f2d152b9813a431721ffdd4815d7f23e0e
AyoDennis <ayodejidennis33@gmail.com>
2025-04-23 22:22:13 +0100

    pyspark

--- spark/pyspark.py
+++ spark/pyspark.py
@@ -1,8 +1,25 @@
+# from pyspark.sql import SparkSession
+
+# JOB_NAME: str = ""
+
+# spark = SparkSession.builder \
+#     .master("spark://localhost[*]") \
+#     .appName(JOB_NAME) \
+#     .getOrCreate()
+
+
 from pyspark.sql import SparkSession
 
-JOB_NAME: str = ""
+spark = SparkSession.builder.appName("demo").getOrCreate()
 
-spark = SparkSession.builder \
-    .master("spark://localhost[*]") \
-    .appName(JOB_NAME) \
-    .getOrCreate()
+df = spark.createDataFrame(
+    [
+        ("sue", 32),
+        ("li", 3),
+        ("bob", 75),
+        ("heo", 13),
+    ],
+    ["first_name", "age"],
+)
+
+df.show()
