f0c268b9816b1e5323eb1b84ead02960de76564f
AyoDennis <ayodejidennis33@gmail.com>
2025-04-23 21:27:49 +0100

    troubleshooting

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,55 +1,55 @@
-# # Create a VPC
-# resource "aws_vpc" "big-data-platform-team-3" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
+# Create a VPC
+resource "aws_vpc" "big-data-platform-team-3" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-#   tags = {
-#     Name = "main"
-#   }
-# }
+  tags = {
+    Name = "main"
+  }
+}
 
-# #Public subnet
+#Public subnet
 
-# resource "aws_subnet" "public_subnet" {
-#   vpc_id                  = aws_vpc.big-data-platform-team-3.id
-#   cidr_block              = "10.0.1.0/24"
-#   availability_zone       = "us-east-1a"
-#   map_public_ip_on_launch = true
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.big-data-platform-team-3.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name = "Main"
-#   }
-# }
+  tags = {
+    Name = "Main"
+  }
+}
 
-# # Internet Gateway
+# Internet Gateway
 
-# resource "aws_internet_gateway" "igw" {
-#   vpc_id = aws_vpc.big-data-platform-team-3.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.big-data-platform-team-3.id
 
-#   tags = {
-#     Name = "main"
-#   }
-# }
+  tags = {
+    Name = "main"
+  }
+}
 
 
-# # Route Table
+# Route Table
 
-# resource "aws_route_table" "route_table" {
-#   vpc_id = aws_vpc.big-data-platform-team-3.id
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.big-data-platform-team-3.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.igw.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
-#   tags = {
-#     Name = "main"
-#   }
-# }
+  tags = {
+    Name = "main"
+  }
+}
 
-# # Subnet to route table association
+# Subnet to route table association
 
-# resource "aws_route_table_association" "public_association" {
-#   subnet_id      = aws_subnet.public_subnet.id
-#   route_table_id = aws_route_table.route_table.id
-# }
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
